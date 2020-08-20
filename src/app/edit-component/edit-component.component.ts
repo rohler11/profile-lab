84a9ca027a6f7d76fb17a7cae06804af5539bac5
rachel <rachelohler@gmail.com>
2020-08-20 10:02:54 -0400

    done-ish

--- src/app/edit-component/edit-component.component.ts
+++ src/app/edit-component/edit-component.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProfileServiceService } from '../profile-service.service';
+import { UserProfile } from '../interfaces/user-profile';
 
 @Component({
   selector: 'app-edit-component',
@@ -11,20 +12,14 @@ import { ProfileServiceService } from '../profile-service.service';
 export class EditComponentComponent implements OnInit {
   constructor(private service: ProfileServiceService, private router: Router) {}
 
-  ngOnInit(): void {}
+  profiles: UserProfile;
+
+  ngOnInit(): void {
+    this.profiles = this.service.getUserProfile();
+  }
 
   editProfileForm(form: NgForm) {
     this.service.setUserProfile(form.value);
     this.router.navigate(['profile']);
   }
-
-  // setUserProfile(form: NgForm) {
-  //   let newProfile: UserProfile = {
-  //     name: form.value.name,
-  //     contact: form.value.contact,
-  //     bio: form.value.bio,
-  //   };
-  //   this.service.setUserProfile(newProfile);
-  //   this.router.navigate(['profile']);
-  // }
 }
